6856bfa0aea8c2f8e24be2d87142ecc58a5b588c
imdeen <ilikemintyicecream.com>
2023-10-10 14:23:30 +0530

    strings

--- 3.strings/7.duplicate_find.c
+++ 3.strings/7.duplicate_find.c
@@ -27,7 +27,25 @@ int multiDuplicates(){
 
 }
 
+int duplicate_bitwise(){
+   char A[]="ffindingf";
+   int b=0,x=0;
+   for(int i=0;A[i]!='\0';i++){
+    x=1;
+    x = x << (A[i]-97);
+    if((x & b)>0){
+        printf("%c is duplicate\n",A[i]);
+        x = (x & b);
+       
+    }else{
+        b = (x | b);
+    }
+
+   }
+    
+}
+
 int main(){
-    multiDuplicates();
+    duplicate_bitwise();
     return 0;
 }
